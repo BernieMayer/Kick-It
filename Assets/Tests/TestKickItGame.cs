f7d2e759297c1fa8343f6bcbdd97573a75153f27
Bernie Mayer <berniemayer94@gmail.com>
2023-11-28 22:57:56 -0700

    Add GameOver test case

--- Assets/Tests/TestKickItGame.cs
+++ Assets/Tests/TestKickItGame.cs
@@ -10,10 +10,22 @@ public class TestKickItGame
     [Test]
     public void TestKickItGameSimplePasses()
     {
-        // Use the Assert class to test conditions
         KickItGame game = ScriptableObject.CreateInstance<KickItGame>();
 
         Assert.That(game != null);
+    }
+
+    [Test]
+    public void TestKickItGameGameOver() {
+        KickItGame game = ScriptableObject.CreateInstance<KickItGame>();
+        KickItGame.Move[] moves = {KickItGame.Move.Jab, KickItGame.Move.Jab};
+
+        game.moves = moves;
+        game.currentMoves = new ArrayList();
+
+        game.AddMove(KickItGame.Move.Cross);
+
+        Assert.That(game.IsGameOver()); 
     }
 
     // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
